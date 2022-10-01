541b6e7625bd08006f0e977c945837b555362459
QANirajP <patel.niraj58.np@gmail.com>
2022-10-01 17:38:51 +0100

    Test Homework

---
+++ src/helloworld/MyName.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class MyName {
+    public static void main(String[] args)
+    {
+        System.out.println("-----------");
+        System.out.print("Niraj");
+        System.out.println(" Patel");
+        System.out.println("-----------");
+    }
+}
---
+++ src/helloworld/PrintNumber.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class PrintNumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c;
+        String name = "print";
+
+        System.out.println(a + b);
+        System.out.println(name);
+    }
